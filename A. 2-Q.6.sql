e87292dbde47e63c2782ca57c17ebe59a65ddca0
Neha KV <itsnehakv@gmail.com>
2026-04-04 19:13:52 +0530

    Update A. 2-Q.6.sql

--- A. 2-Q.6.sql
+++ A. 2-Q.6.sql
@@ -1,3 +1,5 @@
+-- Q. How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the OR operator.
+
 -- What i did (sneaky but worked lol)
 select * from cd.facilities WHERE name ILIKE '%2%';
 
