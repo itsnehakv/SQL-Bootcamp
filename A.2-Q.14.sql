391468d4d544712eb69b7e8d27d8ef60cf84b3b6
Neha KV <itsnehakv@gmail.com>
2026-04-04 19:45:39 +0530

    Create A.2-Q.14.sql

---
+++ A.2-Q.14.sql
@@ -0,0 +1,12 @@
+-- Q. How can you produce a list of the start times for bookings by members named 'David Farrell'?
+
+-- SOLUTION 1: WITH JOIN
+select starttime from cd.bookings INNER JOIN cd.members
+ON cd.bookings.memid=cd.members.memid
+WHERE surname ILIKE 'FArrell' AND firstname ILIKE 'DAVID';
+
+
+-- SOLUTION 2: WITHOUT JOIN
+select starttime from cd.bookings WHERE memid IN
+(select memid from cd.members 
+WHERE surname ILIKE 'farrell' AND firstname ILIKE 'david');
